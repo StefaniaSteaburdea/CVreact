3b2fd13bcb3e7d2efa61518563dd5654fdc2917d
Stefania <stefania.steaburdea@gmail.com>
2021-05-22 21:15:24 +0300

    first page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-const sayHello=()=>{
-   console.log("sayHello");
-};
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import "./App.css";
+import Hobbies from './Hobbies';
+import Skills from './Skills';
+
+const Info=()=>(
+  <div className="grid-container">
+       
+  <div className="blackBox">
+  <h1 className="name">Steaburdea Stefania</h1>
+  <Link to="/skills"><button className="buttons" > Studies and skills</button></Link>
+  <Link to="/hobbies"><button className="buttons" > Hobbies and free time</button></Link>
+  <div className="bottom">
+  <h2 className="h2">Contact :</h2>
+  <p className="p">Email:stefania.steaburdea@gmail.com</p>
+  <p className="p">Phone number: 0764380660</p>
+  <p className="p">Facebook: www.facebook.com/stefania.steaburdea/</p>
+  <p className="p">Github: github.com/StefaniaSteaburdea</p>
+  <p className="p">Instagram: www.instagram.com/stefania.steaburdea/</p>
+  </div>
+  </div>
+  <div className="patternBox">
+  </div>
+</div>
+);
 function App(){
   return(
-     <div>
-       <h1>Steaburdea Stefania</h1>
-       <button onClick={ sayHello}> Hello</button>
-     </div>
+     <Router>
+       <div>
+         <Switch>
+           <Route path="/" exact component={Info}/>
+           <Route path="/skills" exact component={Skills}/>
+           <Route path="/hobbies" exact component={Hobbies}/>
+         </Switch>
+       </div>
+     </Router>
   );
 }
 export default App;
---
+++ src/Hobbies.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+
+function Hobbies(){
+  return(
+     <div className="grid-container">
+       
+       <div className="blackBox">
+       <h1 className="name">Hobbies and free time</h1>
+       <button className="buttons" > Home</button>
+       <button className="buttons" >Studies and Skills </button>
+       </div>
+       <div className="patternBox">
+       </div>
+     </div>
+  );
+}
+export default Hobbies;
